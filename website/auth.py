d2ce9f57659d59d24d718ab6541745c907ef50e7
snigiisteal <154628717+snigiisteal@users.noreply.github.com>
2024-07-04 20:59:09 +0530

    added flask files

---
+++ website/auth.py
@@ -0,0 +1,13 @@
+from flask import Blueprint,render_template,url_for
+
+auth = Blueprint('auth',__name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
+
